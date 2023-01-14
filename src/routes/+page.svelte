320e232e46ff96e48b9cd9ca362d68f1b3c70ffd
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-14 09:18:47 -0600

    add layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<button on:click={() => goto(`https://validator.schema.org/#url=${$page.url.origin}`)}
-	>Validate Schema</button
->
+<button on:click={() => goto(`https://validator.schema.org/#url=${$page.url.origin}`)}>
+	Validate Schema
+</button>
